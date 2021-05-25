880029917a187da1cb00e8ce54238e61a3c5aa00
HTMLissa <melissa_w1994@yahoo.de>
2021-05-25 22:16:51 +0200

    Initialize AVAZ Website

---
+++ avaz.js
@@ -0,0 +1,10 @@
+let myNav = document.querySelector(".navbar");
+
+window.onscroll = function () {
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 20) {
+        myNav.classList.add("nav-colored");
+    }
+    else {
+        myNav.classList.remove("nav-colored");
+    }
+};
