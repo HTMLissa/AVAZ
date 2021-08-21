6b667759e441a888bf8276de6f493c7611e734c5
HTMLissa <melissa_w1994@yahoo.de>
2021-08-21 18:14:03 +0200

    New Layout music.html

--- avaz.js
+++ avaz.js
@@ -1,10 +1,14 @@
 let myNav = document.querySelector(".navbar");
 
 window.onscroll = function () {
-    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 20) {
-        myNav.classList.add("nav-colored");
-    }
-    else {
-        myNav.classList.remove("nav-colored");
-    }
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 20
+  ) {
+    myNav.classList.add("nav-colored");
+  } else {
+    myNav.classList.remove("nav-colored");
+  }
 };
+
+// Visualizer
